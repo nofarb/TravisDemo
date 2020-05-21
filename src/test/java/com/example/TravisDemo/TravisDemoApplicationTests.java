26fb0088526e4c5f707429e8e70c06a829875366
Nofar Bluestein <blue@macbook-pro.fios-router.home>
2020-05-21 15:46:36 -0400

    first commit

---
+++ src/test/java/com/example/TravisDemo/TravisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TravisDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
